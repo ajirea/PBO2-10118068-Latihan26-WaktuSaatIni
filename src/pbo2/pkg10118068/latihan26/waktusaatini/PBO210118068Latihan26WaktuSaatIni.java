31a4c5f7482f11a8f9b59d85a27ea837a912e77e
Satria Aji Putra <satriamaxt@gmail.com>
2019-10-03 16:09:15 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan26/waktusaatini/PBO210118068Latihan26WaktuSaatIni.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan26.waktusaatini;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : program menampilkan waktu hari ini, dari mulai hari,
+ * tanggal, bulan, tahun, jam, menit, dan detik
+ */
+public class PBO210118068Latihan26WaktuSaatIni {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // make date object
+        Date date = new Date();
+        
+        // make date format object
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "EEEE, d MMMMM Y H:m:s",
+                new Locale("id")
+        );
+        
+        // output 
+        String hasil = dateFormat.format(date);
+        System.out.println("Hari ini adalah hari : " + hasil);
+    }
+    
+}
